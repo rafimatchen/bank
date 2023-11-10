f2536fc090834a69c9f84ce698c3a959d8ee4b13
Rafi Matchen <rafimatchen@gmail.com>
2023-11-10 16:15:20 -0500

    correctly downloads holdings csv

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ driver.get(config["url"])
 
 title = driver.title
 
-driver.implicitly_wait(0.5)
+driver.implicitly_wait(5)
 
 username_input = driver.find_element(by=By.ID, value="username")
 password_input = driver.find_element(by=By.ID, value="uapPassword")
@@ -23,5 +23,17 @@ submit_button = driver.find_element(
 username_input.send_keys(config["login"])
 password_input.send_keys(config["password"])
 submit_button.click()
+
+accounts_button = driver.find_elements(by=By.CSS_SELECTOR, value="td-wb-menu-item")[1]
+accounts_button.click()
+
+holdings_button = driver.find_element(by=By.CSS_SELECTOR, value='[href="#/page/account/holdings"]')
+holdings_button.click()
+
+export_button = driver.find_element(by=By.CSS_SELECTOR, value="td-wb-toolbar-item-export")
+export_button.click()
+
+csv_button = driver.find_element(by=By.ID, value="td-wb-toolbar-item-export-CSV")
+csv_button.click()
 
 input()
